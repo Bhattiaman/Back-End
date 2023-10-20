78a7927d64e1fa990f39dfbf5c5fbee71207968d
Bhattiaman <bhattiaman@kali.Bhattiaman>
2023-10-20 17:38:19 +0530

    tut

--- Day_30 /script.js
+++ Day_30 /script.js
@@ -1,11 +1,1 @@
-// let n = 5;
-// for(let i = 0; i< n; i++){
-//     console.log("hello : ", i);
-// }
-
-console.log(process.argv);
-
-let args = process.argv;
-for(let i = 2; i < args.length; i++){
-    console.log("Hello to ", args[i]);
-} 
+const someValue = require("./math.js");
