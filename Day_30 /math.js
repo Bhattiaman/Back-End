a03bbb41f0824a923ee41e04746c8e9dd39bd419
Bhattiaman <bhattiaman@kali.Bhattiaman>
2023-10-20 17:51:06 +0530

    tut

--- Day_30 /math.js
+++ Day_30 /math.js
@@ -2,5 +2,18 @@ const sum = (a,b) => a + b;
 const mul = (a,b) => a * b;
 const g = 9.8;
 const pi = 3.14
+module.exports = {
+    sum: sum,
+    mul: mul,
+    g: g,
+    pi: pi
+}
 
-module.exports = 123;
+let obj = {
+    sum: sum,
+    mul: mul,
+    g: g,
+    pi: pi
+}
+
+module.exports = obj;
--- Day_30 /script.js
+++ Day_30 /script.js
@@ -1,3 +1,4 @@
 const someValue = require("./math.js");
-
 console.log(someValue);
+console.log(someValue.pi);
+console.log(someValue.sum(3,4));
